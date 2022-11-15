b3a935916e4f5cecef518e803ca3d2e5f2c7e990
Dave DeSandro <desandrocodes@gmail.com>
2022-11-15 14:44:12 -0500

    ⚙️ lint self; fix lints

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 module.exports = {
-  "env": {
-    "browser": true,
+  'env': {
+    'browser': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "google"
+  'extends': [
+    'eslint:recommended',
+    'google',
   ],
-  "globals": {
+  'globals': {
   },
-  "ignorePatterns": ["**/*.bundle.js", "**/*.min.js", "node_modules/"],
-  "parserOptions": {
-    "ecmaVersion": 7,
-    "sourceType": "module"
+  'ignorePatterns': ['**/*.bundle.js', '**/*.min.js', 'node_modules/'],
+  'parserOptions': {
+    'ecmaVersion': 7,
+    'sourceType': 'module',
   },
-  "plugins": [
-    "simple-import-sort"
+  'plugins': [
+    'simple-import-sort',
   ],
-  "rules": {
-    "arrow-parens": ["error", "always"],
-    "camelcase": "warn",
-    "comma-dangle": ["warn", "always-multiline"],
-    "generator-star-spacing": "error",
-    "indent": ["error", 2],
-    "jsx-quotes": ["error", "prefer-double"],
-    "max-len": [
+  'rules': {
+    'arrow-parens': ['error', 'always'],
+    'camelcase': 'warn',
+    'comma-dangle': ['warn', 'always-multiline'],
+    'generator-star-spacing': 'error',
+    'indent': ['error', 2],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'max-len': [
       0,
       {
-        "ignoreComments": true,
-        "ignoreTrailingComments": true,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true,
-        "ignoreRegExpLiterals": true,
-        "ignorePattern": true
-      }
+        'ignoreComments': true,
+        'ignoreTrailingComments': true,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true,
+        'ignorePattern': true,
+      },
     ],
-    "new-cap": "warn",
-    "no-alert": "error",
-    "no-console": "warn",
-    "no-invalid-this": "error",
-    "no-negated-condition": "warn",
-    "no-unused-vars": [ "error", {
-      "args": "after-used"
-    } ],
-    "object-curly-spacing": ["error", "never"],
-    "quotes": ["error", "single"],
-    "require-jsdoc": [
-      "error",
+    'new-cap': 'warn',
+    'no-alert': 'error',
+    'no-console': 'warn',
+    'no-invalid-this': 'error',
+    'no-negated-condition': 'warn',
+    'no-unused-vars': ['error', {
+      'args': 'after-used',
+    }],
+    'object-curly-spacing': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'require-jsdoc': [
+      'error',
       {
-        "require": {
-          "FunctionDeclaration": true
-        }
-      }
+        'require': {
+          'FunctionDeclaration': true,
+        },
+      },
     ],
-    "simple-import-sort/imports": "error",
+    'simple-import-sort/imports': 'error',
   },
 };
